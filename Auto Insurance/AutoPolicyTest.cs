84b98ca87d351de92805d7d50dc2626566bc084a
jacob gishwiller <sirbruce13@yahoo.com>
2022-02-17 08:38:00 -0700

    fixed

--- Auto Insurance/AutoPolicyTest.cs
+++ Auto Insurance/AutoPolicyTest.cs
@@ -2,11 +2,30 @@
 
 namespace Auto_Insurance
 {
-   class Program
+   class AutoPolicyTest
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         //creat two AutoPolicy objects
+         AutoPolicy policy1 = new AutoPolicy(11111111, "Toyota Camry", "NJ");
+         AutoPolicy policy2 = new AutoPolicy(22222222, "Ford Fusion", "ME");
+
+         //display wether each policy is in a no-fault state
+         PolicyInNoFaultState(policy1);
+         PolicyInNoFaultState(policy2);
+      }
+
+      //method that displays whether an AutoPolicy
+      //is in a state with no-fault auto insurance
+      public static void PolicyInNoFaultState(AutoPolicy policy)
+      {
+         Console.WriteLine("The auto policy");
+         Console.WriteLine($"Account #: {policy.AccountNumber};");
+         Console.WriteLine($"Car: {policy.MakeAndModel};");
+         Console.WriteLine($"State {policy.State}; ");
+         Console.WriteLine($"({(policy.IsNoFaultState ? "is": " is not ")})");
+         Console.WriteLine("a no-fault state\n");
+         
       }
    }
 }
